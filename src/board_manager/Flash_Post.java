a38a54acaddfb1b3d8cd81a8789efea335800841
Pedro Pablo García Pozo <i82gapop@uco.es>
2020-10-14 00:17:57 +0200

    Actualizacion

--- src/board_manager/Flash_Post.java
+++ src/board_manager/Flash_Post.java
@@ -6,6 +6,7 @@ import java.util.Date;
 
 public class Flash_Post extends Post{
 
+    protected Date date_start;
     protected Date date_end;
 
     public Flash_Post(int identifier, String title, String body, Contact owner, Date publication){
@@ -13,8 +14,10 @@ public class Flash_Post extends Post{
         super(identifier, title, body, owner, publication);
     }
     
+    public Date getDateStart() {return date_start;}
     public Date getDateEnd() {return date_end;}
 
+    public void setDateStart(Date date_start) {this.date_start = date_start;}
     public void setDateEnd(Date date_end) {this.date_end = date_end;}
 
     public String toString(){
